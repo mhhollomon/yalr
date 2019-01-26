1cd93c147c7e53e717f12b6a607defa60643a9d6
Mark Hollomon <mhhollomon@gmail.com>
2019-01-25 20:55:15 -0500

    Add CL option to dump state table to file
    implement Issue #6
    Provide temp fix for Issue #10

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@
 struct CLIOptions {
     std::string output_file;
     std::string translate;
+    std::string state_file;
     std::string input_file;
     bool help = false;
 };
@@ -41,6 +42,8 @@ CLIOptions parse_commandline(int argc, char**argv) {
         options.add_options()
             ("h,help", "Print help message", cxxopts::value(clopts.help))
             ("o,output-file", "File in which to put the main output", cxxopts::value(clopts.output_file))
+            ("S,state-table", "File in which to put the state table", 
+                cxxopts::value(clopts.state_file)->implicit_value("-NONE :^-") )
             ("t,translate", "Output the grammar in another format", cxxopts::value(clopts.translate))
             ;
         options.add_options("positionals")
@@ -125,15 +128,25 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Good input\n";
 
+    /*
     std::cout << "------ AST ------\n";
     yalr::ast::pretty_print(tree, std::cout);
     std::cout << "------ AST ------\n";
+    */
 
     auto ana_tree = yalr::analyzer::analyze(tree);
+    if (not ana_tree) {
+        // the analyzer should be putting out various errors
+        // so we just need to stop.
+        exit(1);
+    }
 
+    /*
     std::cout << "------ ANALYZE ------\n";
     yalr::analyzer::pretty_print(*ana_tree, std::cout);
     std::cout << "------ ANALYZE ------\n";
+    */
+
 
     std::string outfilename;
     if (not clopts.output_file.empty()) {
@@ -149,7 +162,7 @@ int main(int argc, char* argv[]) {
             std::ofstream code_out(outfilename, std::ios_base::out);
             yalr::translate::grammophone(*ana_tree).output(code_out);
         } else {
-            std::cerr << "Unkown format '" << format << "'\n";
+            std::cerr << "Unknown format '" << format << "'\n";
             exit(1);
         }
 
@@ -158,9 +171,18 @@ int main(int argc, char* argv[]) {
 
     auto lrtbl = yalr::tablegen::generate_table(*ana_tree);
 
-    std::cout << "------ TABLEGEN ------\n";
-    yalr::tablegen::pretty_print(*lrtbl, std::cout);
-    std::cout << "------ TABLEGEN ------\n";
+    std::string state_file_name;
+    if (not clopts.state_file.empty()) {
+        if (clopts.state_file == "-NONE :^-") {
+            state_file_name = outfilename + ".state.txt";
+        } else {
+            state_file_name = clopts.state_file;
+        }
+        std::cout << "--- Generating state table into " << state_file_name << "\n";
+        std::ofstream state_out(state_file_name, std::ios_base::out);
+        yalr::tablegen::pretty_print(*lrtbl, state_out);
+    }
+
 
     std::cout << "--- Generating code into " << outfilename << "\n";
     std::ofstream code_out(outfilename, std::ios_base::out);
